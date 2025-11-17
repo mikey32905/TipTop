ab30ee368c80aedbe77922f2fc5bca33dc9d8e96
Mike Williams <mikey32905@gmail.com>
2025-11-17 06:15:05 -0500

    added netlify.build.sh

---
+++ netlify.build.sh
@@ -0,0 +1,19 @@
+﻿#!/usr/bin/env bash
+set -e
+
+# Change into a temporary directory so we can download + install .NET
+pushd /tmp
+
+# Download Microsoft install script
+wget https://dot.net/v1/dotnet-install.sh
+
+# Make the script executable
+chmod u+x /tmp/dotnet-install.sh
+
+/tmp/dotnet-install.sh --channel 8.0
+
+# Return to project directory
+popd
+
+# Now we can publish (since we've installed .NET in this environment)
+dotnet publish -c Release -o release
